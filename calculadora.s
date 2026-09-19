76d5907e2cacdffcc9de93c99c01f0c2dcd60eb8
Nicvolty <manuelmendoza2201@gmail.com>
2026-09-18 19:47:55 -0600

    potencia pero sin verificar el exponente negativo

--- calculadora.s
+++ calculadora.s
@@ -24,6 +24,10 @@ msg_error_division_cero:
     .ascii "[ERROR division] -> No se puede dividir entre cero"
     msg_error_division_cero_len = . - msg_error_division_cero
 
+msg_error_exponente:
+    .ascii "[ERROR exponente] -> El exponente debe ser entero no negativo\n"
+    msg_error_exponente_len = . - msg_error_exponente
+
 newline:
     .ascii "\n"
 
@@ -71,6 +75,9 @@ _start:
     cmp w0, '4'
     beq Division
 
+    cmp w0, '5'
+    beq potencia
+
     cmp w0, '7'
     beq exit
 
@@ -90,6 +97,10 @@ multi:
 
 Division:
     mov x22, #4    
+    b read_numbers
+    
+potencia:
+    mov x22, #5
 
 read_numbers:
     // imprimir mensaje de primer numero
@@ -120,6 +131,9 @@ read_numbers:
     cmp x22, #4   // comparar registro x22 con el numero 4 ya que ese es el asignado para la division
     beq dividir
 
+    cmp x22, #5   // Comparar registro x22 para ver si es potencia
+    beq calcular_potencia
+
     cbnz x22, subtract
     add x20, x20, x21
     b print_result
@@ -135,6 +149,33 @@ multiplicar:
 dividir:
     cbz x21, error_division_cero
     sdiv x20 , x20, x21
+    b print_result
+
+calcular_potencia:
+    // Validar que el exponente no sea negativo
+    cmp x21, #0
+    blt error_exponente_negativo
+
+    // Inicializar el acumulador x9 en 1
+    mov x9, #1
+
+loop_potencia:
+    // Si el exponente llegó a 0, terminamos
+    cbz x21, fin_potencia
+
+    // Multiplicar acumulador por la base: x9 = x9 * x20
+    mul x9, x9, x20
+
+    // Decrementar el exponente: x21 = x21 - 1
+    sub x21, x21, #1
+
+    // Volver a iterar
+    b loop_potencia
+
+fin_potencia:
+   // se carga en x20 el resultado
+    mov x20, x9
+    b print_result
 
 print_result:
     // imprimir el resultado
@@ -189,3 +230,10 @@ error_division_cero:
     mov x2, #1
     bl print
     b _start
+
+ 
+error_exponente_negativo:
+    ldr x1, =msg_error_exponente
+    mov x2, msg_error_exponente_len
+    bl print
+    b _start   
